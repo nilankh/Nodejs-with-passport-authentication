44fc7582da58ecc6c5766c7f16b9441c1c99c723
Nilank <Nilanknikhil@gmail.com>
2020-05-07 23:11:05 +0530

    setted routes for user js basic

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 //ROutes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
 const PORT = 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
