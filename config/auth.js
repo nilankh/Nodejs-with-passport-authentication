edb82b8421ba14fbf3315002747562793bbd9b2e
Nilank <Nilanknikhil@gmail.com>
2020-05-08 21:30:41 +0530

    completed whole authneticaton accordign to me completed project

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,9 @@
-module.exports = 
+module.exports = {
+    ensureAuthenticated: function(req, res, next){
+        if(req.isAuthenticated()){
+            return next();
+        }
+        req.flash('error_msg','Please log in to view this resource');
+        res.redirect('./users/login');
+    }
+}
